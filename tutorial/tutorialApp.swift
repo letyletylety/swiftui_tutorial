80f422b40dc0013b97e6b4e3c7342419365bf943
Lety <taej0127@gmail.com>
2021-06-11 17:57:55 +0900

    Initial Commit

---
+++ tutorial/tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  tutorialApp.swift
+//  tutorial
+//
+//  Created by 강태진 on 2021/06/11.
+//
+
+import SwiftUI
+
+@main
+struct tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
